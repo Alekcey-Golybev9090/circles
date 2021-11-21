90bdd2bd006179908d9a40adb8e3cb6b3cba518e
Igor Vorobyev <67874208+Alekcey-Golybev9090@users.noreply.github.com>
2021-11-21 21:33:04 +0300

    third commit

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
+from random import randrange
 
 
-# Press the green button in the gutter to run the script.
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.do_paint = False
+        self.btn_create.clicked.connect(self.create_circle)
+
+    def create_circle(self):
+        self.do_paint = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            qp.setPen(QColor(255, 242, 0))
+            r = randrange(5, 200)
+            qp.drawEllipse(300, 350, 2 * r, 2 * r)
+            qp.end()
+            self.do_paint = False
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    ex = MyWindow()
+    ex.show()
+    sys.exit(app.exec_())
